2cf3c1f551358ac9fca5ff1badd795db8e03d6fd
Carlo Fontanos <carl.fontanos@gmail.com>
2016-11-16 16:58:03 +0800

    Integrated direct HTTP calls using fetch. Data is noe fetched from a server.

--- Modules/Backend.js
+++ Modules/Backend.js
@@ -1,51 +1,18 @@
-var hikes = [
-    {
-        id: 0,
-        name: "Tricky Trails",
-        location: "Lakebed, Utah",
-        distance: 10.4,
-        rating: 4,
-        comments: "This hike was nice and hike-like. Glad I didn't bring a bike."
-    },
-    {
-        id: 1,
-        name: "Mondo Mountains",
-        location: "Black Hills, South Dakota",
-        distance: 20.86,
-        rating: 3,
-        comments: "Not the best, but would probably do again. Note to self: don't forget the sandwiches next time."
-    },
-    {
-        id: 2,
-        name: "Pesky Peaks",
-        location: "Bergenhagen, Norway",
-        distance: 8.2,
-        rating: 5,
-        comments: "Short but SO sweet!!"
-    },
-    {
-        id: 3,
-        name: "Rad Rivers",
-        location: "Moriyama, Japan",
-        distance: 12.3,
-        rating: 4,
-        comments: "Took my time with this one. Great view!"
-    },
-    {
-        id: 4,
-        name: "Dangerous Dirt",
-        location: "Cactus, Arizona",
-        distance: 19.34,
-        rating: 2,
-        comments: "Too long, too hot. Also that snakebite wasn't very fun."
-    }
-];
-
 /* Returns a Promise that represents an array of hike objects */
 function getHikes() {
 	return new Promise(function(resolve, reject) {
         setTimeout(function() {
-            resolve(hikes);
+			fetch('http://carlofontanos.com/api/fuse.php', {
+				method: 'POST'
+			}).then(function(response) {
+				return response.json();    // This returns a promise
+			}).then(function(responseObject) {
+				// Do something with the result
+				var hikes = responseObject;
+				resolve(hikes);
+			}).catch(function(err) {
+				console.log("An error occured --- " + err);
+			});
         }, 0);
     });
 }
@@ -53,19 +20,29 @@ function getHikes() {
 function updateHike(id, name, location, distance, rating, comments) {
     return new Promise(function(resolve, reject) {
         setTimeout(function() {
-            for (var i = 0; i < hikes.length; i++) {
-                var hike = hikes[i];
-                if (hike.id == id) {
-                    hike.name = name;
-                    hike.location = location;
-                    hike.distance = distance;
-                    hike.rating = rating;
-                    hike.comments = comments;
-                    break;
-                }
-            }
-
-            resolve();
+            fetch('http://carlofontanos.com/api/fuse.php', {
+				method: 'POST'
+			}).then(function(response) {
+				return response.json();    // This returns a promise
+			}).then(function(responseObject) {
+				// Do something with the result
+				var hikes = responseObject;
+				
+				for (var i = 0; i < hikes.length; i++) {
+					var hike = hikes[i];
+					if (hike.id == id) {
+						hike.name = name;
+						hike.location = location;
+						hike.distance = distance;
+						hike.rating = rating;
+						hike.comments = comments;
+						break;
+					}
+				}
+				resolve();
+			}).catch(function(err) {
+				console.log("An error occured --- " + err);
+			});
         }, 0);
     });
 }
